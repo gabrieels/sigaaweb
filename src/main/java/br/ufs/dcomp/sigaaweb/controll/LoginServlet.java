aec2331803d871b0fe9f0f9f9b141b90930504f6
gabriel <carlos.nunes@dcomp.ufs.br>
2017-09-17 02:11:25 -0300

    Update LoginServlet

--- src/main/java/br/ufs/dcomp/sigaaweb/controll/LoginServlet.java
+++ src/main/java/br/ufs/dcomp/sigaaweb/controll/LoginServlet.java
@@ -9,19 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.ufs.dcomp.sigaaweb.dao.AlunoDao;
+import br.ufs.dcomp.sigaaweb.model.AlunoBean;
+
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
+	AlunoDao alunoDao = new AlunoDao();
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String nome = req.getParameter("nome");
+		AlunoBean alunoBean = new AlunoBean();
+		
+		long matricula = Long.parseLong(req.getParameter("usermatricula"));
+		
+		alunoBean = alunoDao.findByMatricula(matricula);
 
 		resp.setContentType("text/html");
 
 		PrintWriter out = resp.getWriter();
-		out.println("Ola: " + nome);
+		out.println("Ola: " + alunoBean.getNomeAluno());
 
 	}
 
