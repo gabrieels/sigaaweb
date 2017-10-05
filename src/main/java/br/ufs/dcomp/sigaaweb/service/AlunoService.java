59b6af18820d51c15596f4987d850ad36c01bb31
gabriel <carlos.nunes@dcomp.ufs.br>
2017-10-05 02:33:11 -0300

    Update class AlunoService

--- src/main/java/br/ufs/dcomp/sigaaweb/service/AlunoService.java
+++ src/main/java/br/ufs/dcomp/sigaaweb/service/AlunoService.java
@@ -1,15 +1,36 @@
 package br.ufs.dcomp.sigaaweb.service;
 
+import java.util.List;
+
 import br.ufs.dcomp.sigaaweb.dao.AlunoDao;
+import br.ufs.dcomp.sigaaweb.dao.TurmaDao;
 import br.ufs.dcomp.sigaaweb.model.AlunoBean;
+import br.ufs.dcomp.sigaaweb.model.TurmaBean;
 
 public class AlunoService {
 	AlunoDao alunoDao = new AlunoDao();
+	TurmaDao turmaDao = new TurmaDao();
 	AlunoBean alunoBean;
+	TurmaBean turmaBean;
+	List<AlunoBean> alunoBeans;
+	List<TurmaBean> turmaBeans;
 
 	public AlunoBean autenticar(long matricula) {
 		alunoBean = alunoDao.findByMatricula(matricula);
-		
+
 		return alunoBean;
+	}
+
+	public AlunoBean buscarAluno(long matricula) {
+
+		return this.alunoDao.findByMatricula(matricula);
+	}
+
+	public List<AlunoBean> listarAlunos() {
+		return this.alunoDao.findAll();
+	}
+	
+	public List<AlunoBean> buscarAlunosDisciplina(String codDisciplina) {
+		return this.alunoDao.findByDisciplina(codDisciplina);
 	}
 }
