a28c9bffdeeb58122b60602eb4d83bd2b48685ba
gabriel <carlos.nunes@dcomp.ufs.br>
2017-10-10 22:30:09 -0300

    Update class HomeServlet

--- src/main/java/br/ufs/dcomp/sigaaweb/controll/HomeServlet.java
+++ src/main/java/br/ufs/dcomp/sigaaweb/controll/HomeServlet.java
@@ -17,42 +17,42 @@ import br.ufs.dcomp.sigaaweb.service.AlunoService;
 import br.ufs.dcomp.sigaaweb.service.SemestreService;
 
 /**
-* Servlet implementation class HomeServlet
-*/
+ * Servlet implementation class HomeServlet
+ */
 @WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
-	public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private List<SemestreBean> semestre;
-	private SemestreService semestreService = new SemestreService();
-	private AlunoService alunoService = new AlunoService();
+public class HomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private List<SemestreBean> semestre;
+    private SemestreService semestreService = new SemestreService();
+    private AlunoService alunoService = new AlunoService();
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.semestre = semestreService.buscarSemestre();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.semestre = semestreService.buscarSemestre();
 
-		HttpSession session = req.getSession();
-		session.setAttribute("semestre", this.semestre);
+        HttpSession session = req.getSession();
+        session.setAttribute("semestre", this.semestre);
 
-		resp.sendRedirect("home/home.jsp");
-	}
+        resp.sendRedirect("home/home.jsp");
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter out = null;
-		try {
-			out = resp.getWriter();
-			String[] data = req.getParameter("primary").split(";");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = null;
+        try {
+            out = resp.getWriter();
+            String[] data = req.getParameter("primary").split(";");
 
-			AlunoBean alunoBean = (AlunoBean) req.getSession().getAttribute("alunoLogado");
-			List<AlunoBean> alunoBeans = alunoService.buscarAlunosByDisciplinaByTurma(data[0], data[1]);
+            AlunoBean alunoBean = (AlunoBean) req.getSession().getAttribute("alunoLogado");
+            List<AlunoBean> alunoBeans = alunoService.buscarAlunosByDisciplinaByTurma(data[0], data[1]);
 
-			alunoBeans.add(alunoBean);
+            alunoBeans.add(alunoBean);
 
-			req.setAttribute("alunosTurma", alunoBeans);
+            req.setAttribute("alunosTurma", alunoBeans);
 
-			req.getRequestDispatcher("home/turma.jsp").forward(req, resp);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+            req.getRequestDispatcher("home/turma.jsp").forward(req, resp);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
